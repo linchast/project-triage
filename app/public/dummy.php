3aac458e02df100bd6f864adf363b59b83b65c7a
linchast <54513537+linchast@users.noreply.github.com>
2019-09-09 13:04:07 -0400

    Added Dummy Data for Patient Table

---
+++ app/public/dummy.php
@@ -0,0 +1,25 @@
+[
+  {
+    "patientGuid": "01",
+    "firstName": "Jeff",
+    "lastName": "Black",
+    "dob": "2012-08-01",
+    "sexAtBirth": "M"
+  },
+
+  {
+    "patientGuid": "02",
+    "firstName": "Lindsey",
+    "lastName": "Chastain",
+    "dob": "1997-04-04",
+    "sexAtBirth": "F"
+  },
+
+  {
+    "patientGuid": "03",
+    "firstName": "Ashley",
+    "lastName": "Chastain",
+    "dob": "1999-07-11",
+    "sexAtBirth": "F"
+  }
+]
