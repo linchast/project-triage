afca3ef180ae8c9b67950649d309187876a15f14
linchast <54513537+linchast@users.noreply.github.com>
2019-09-15 16:54:20 -0400

    Updated JS from Class

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,21 +1,52 @@
 var waitingApp = new Vue({
-  el: '#patientWaitingApp', //element
-  data: { //calling the data
-
-    patients: [ ]
+  el: '#patientWaitingApp',
+  data: {
+    patients: []
   },
-  
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then( response => response.json() )
       .then( json => {waitingApp.patients = json})
-      // .catch ( function goes here)
       ;
-      thenDoThis();
+
+      // Means the same at this
+      // fetch('https://randomuser.me/api/')
+      // .then(function(response) {return response.json()})
+      // .then(function(json) {waitingApp.people = json});
+
     }
   },
   created() {
     this.fetchPatients();
   }
 })
+
+
+
+
+
+
+// var waitingApp = new Vue({
+//   el: '#patientWaitingApp',
+//   data: {
+//     patients: []
+//   },
+//   methods: {
+//     fetchPatients() {
+//       fetch('dummy.php')
+//       .then(response => response.json())
+//       .then(json => {waitingApp.patients = json});
+//
+//       // Means the same at this
+//       // fetch('https://randomuser.me/api/')
+//       // .then(function(response) {return response.json()})
+//       // .then(function(json) {waitingApp.people = json});
+//
+//     }
+//   },
+//   created: function() {
+//     this.fetchPatients();
+//   }
+//
+// })
