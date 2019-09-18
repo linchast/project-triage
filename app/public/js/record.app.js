9c5a5feba419eeb4827d2d90ad99194de9ed6c0f
linchast <54513537+linchast@users.noreply.github.com>
2019-09-18 12:29:22 -0400

    Blue 9/16 Commits

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,27 +1,16 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    person: [
-      {
-
-      }
-    ]
+    patients: []
   },
   methods: {
     fetchPatients() {
-      fetch('https://randomuser.me/api/')
-      .then( response => response.json() )
-      .then( json => {patientRecordApp.person = json.results[0]})
-      ;
-
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {waitingApp.people = json});
-
+      fetch('dummy.php')
+      .then(response => response.json())
+      .then(json => { patientRecordApp.patients = json })
     }
   },
   created() {
     this.fetchPatients();
   }
-})
+});
--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,29 +1,26 @@
+
 var waitingApp = new Vue({
-  el: '#patientWaitingApp',
-  data: {
-    person: [
-      {
+el: '#patientWaitingApp',
+data: {
+  patients: []
+},
+methods: {
+  fetchPatients() {
+    fetch('dummy.php')
+    .then( response => response.json() )
+    .then( json => {waitingApp.patients = json})
+    ;
 
-      }
-    ]
-  },
-  methods: {
-    fetchPatients() {
-      fetch('https://randomuser.me/api/')
-      .then( response => response.json() )
-      .then( json => {waitingApp.person = json.results[0]})
-      ;
+    // Means the same at this
+    // fetch('https://randomuser.me/api/')
+    // .then(function(response) {return response.json()})
+    // .then(function(json) {waitingApp.people = json});
 
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {waitingApp.people = json});
-
-    }
-  },
-  created() {
-    this.fetchPatients();
   }
+},
+created() {
+  this.fetchPatients();
+}
 })
 
 
