6a05f301bbaa2e366f522f74dab50b9af1964465
linchast <54513537+linchast@users.noreply.github.com>
2019-09-18 13:16:51 -0400

    class 9/18/2019. Still need to update the Click button on HTML for table rows

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -20,7 +20,17 @@ var patientRecordApp = new Vue({
       //fetch(url, {method: 'post', data: this.formPatient})
       //.then(...)
       this.patients.push(this.formPatient); //take object and add to table
-    }
+      this.formPatient = { //creates a new instance of the form
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+        }
+      },
+      handleRecordClick(patient) {
+        //TODO: pass this data to another Vue App
+        console.log(patient);
+      }
   },
   created() {
     this.fetchPatients();
