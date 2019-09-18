55f4481442d6152b836ba2a1fa51385f086cced9
linchast <54513537+linchast@users.noreply.github.com>
2019-09-18 13:01:37 -0400

    Created form and button to submit new patients to the table

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,13 +1,25 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      sexAtBirth: ''
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event) {
+      //TODO: Post data to API
+      //fetch(url, {method: 'post', data: this.formPatient})
+      //.then(...)
+      this.patients.push(this.formPatient); //take object and add to table
     }
   },
   created() {
